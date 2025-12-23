afe02dac67feaaf79593a8f77cc35dd3a04ff05a
drashtisiapara2012 <drashti.sitapara@marutitech.com>
2025-12-23 16:32:28 +0530

    JS done

--- JavaScript/Asynchronous/AsyncAwait.js
+++ JavaScript/Asynchronous/AsyncAwait.js
@@ -15,15 +15,16 @@
 // }
 // async function displayUser() {
 //   try {
-//     const userId = await getUserId();
+//     const userId =  await getUserId();
 
-//     const userDetails = await getUserDetails(userId);
+//     const userDetails =  await getUserDetails(userId);
 //     console.log("User Details:", userDetails);
 //   } catch (error) {
 //     console.error("Error:", error);
 //   }
 // }
 // displayUser(); 
+
 
 //Removing async/await does not throw a syntax or runtime error, but it causes a logical error because promises are not resolved, resulting in pending promises instead of actual values.
 function getUserId() {
