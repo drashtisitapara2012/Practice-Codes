0785a0d72a3adf1f3091a5e68816be0f1f5a1387
drashtisiapara2012 <drashti.sitapara@marutitech.com>
2026-01-01 17:50:06 +0530

    calculator-demo added

--- React/my-react-app/src/class.jsx
+++ React/my-react-app/src/class.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// The state object is where you store property values that belongs to the component.
+// The state object is where you store property values that belongs to the component. When the state object changes, the component re-renders.
 
 class Car extends React.Component {
   constructor(props) {
--- React/my-react-app/src/main.jsx
+++ React/my-react-app/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import JSXDemo from './JSXDemo.jsx'
-import Car from './class.jsx'
+//import Car from './class.jsx'
+import {Garage,Parent} from './props.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <>
-    <JSXDemo />
-    <Car />
+    {/* <JSXDemo />
+    <Car /> */}
+    <Garage/>
+    <Parent/>
   </>
 )
