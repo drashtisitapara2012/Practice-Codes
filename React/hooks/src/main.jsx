6f361c6c80e7fbe7209f6045508bf480a7334457
drashtisiapara2012 <drashti.sitapara@marutitech.com>
2026-01-07 09:45:24 +0530

    custom hook

--- React/hooks/src/main.jsx
+++ React/hooks/src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import UseStateDemo from './useState.jsx'
 import UseEffectDemo from './useEffect.jsx'
-import UseContextDemo from './useContext.jsx'
+import UseContextDemo from './UseContext/useContext.jsx'
 import Timer from './useRef.jsx'
 import Counter from './useReducer.jsx'
 import UseCallbackDemo from './useCallback.jsx'
 import UseMemoDemo from './useMemo.jsx'
 import CustomHooksDemo from './customHooks.jsx'
-import FormDemo from './form.jsx'
-import LargeScreen from './LargeScreen.jsx'
+import FormDemo from './CustomHook/form.jsx'
+import LargeScreen from './ResizeHook/LargeScreen.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
